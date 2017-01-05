6acca88640bdd71f529f08a9b35b2c0cdd82727f
Hideyo Yoshida <aquaviter@gmail.com>
2017-01-05 16:54:35 +0900

    update version

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.json({ message: 'Hello, World!', version: '1.0' });
+    res.json({ message: 'Hello, World!', version: '2.0' });
 });
 
 app.use('/api', router);
